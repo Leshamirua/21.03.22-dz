531f97af78d91b0efd9bf17708d14794a928da4a
Leshamir <anastasy34@gmail.com>
2023-03-24 15:09:08 +0200

    1

--- 21.03.22 dz/Client.cs
+++ 21.03.22 dz/Client.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _21._03._22_dz
 {
-   internal class Program
+    internal class Client
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
